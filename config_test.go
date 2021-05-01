293a50821e33d49834a58dbd490ab18b01b1389f
Nanang Suryadi <nanang.jobs@gmail.com>
2021-05-02 02:30:53 +0700

    feat: config func and test

---
+++ config_test.go
@@ -0,0 +1,81 @@
+package valkyrie
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type constants struct {
+	App struct {
+		Name         string         `yaml:"name"`
+		Port         int            `yaml:"port"`
+		ReadTimeout  int            `yaml:"read_timeout"`
+		WriteTimeout int            `yaml:"write_timeout"`
+		Timezone     string         `yaml:"timezone"`
+		Debug        bool           `yaml:"debug"`
+		Env          string         `yaml:"env"`
+		SecretKey    string         `yaml:"secret_key"`
+		ExpireIn     *time.Duration `yaml:"expire_in"`
+	} `yaml:"App"`
+
+	DB struct {
+		DsnMain string `yaml:"dsn_main" env:"DSN_MAIN"`
+	}
+}
+
+func TestConfig(t *testing.T) {
+	var cfg constants
+	err := Config(ConfigOpts{
+		Config:    &cfg,
+		Filenames: []string{"app.test.yaml"},
+		Paths:     []string{".", "./config"},
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, 8778, cfg.App.Port)
+}
+
+func TestConfigPathFail(t *testing.T) {
+	var cfg constants
+	err := Config(ConfigOpts{
+		Config:    &cfg,
+		Filenames: []string{"app.test.yaml"},
+		Paths:     []string{"./config"},
+	})
+
+	assert.Error(t, err)
+}
+
+func TestConfigEnv(t *testing.T) {
+	defer os.Clearenv()
+	var cfg constants
+	val := "host=localhost port=5999 user=root password=root123 dbname=dbroot sslmode=disable"
+
+	err := Config(ConfigOpts{
+		Config:    &cfg,
+		Filenames: []string{"app.test.yaml"},
+		Paths:     []string{".", "./config"},
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, val, cfg.DB.DsnMain)
+}
+
+func TestConfigFunc(t *testing.T) {
+	defer os.Clearenv()
+	var cfg constants
+	val := "host=localhost port=5999 user=root password=root123 dbname=dbroot sslmode=disable"
+	_ = os.Setenv("DSN_MAIN", val)
+
+	err := Config(ConfigOpts{
+		Config:    &cfg,
+		Filenames: []string{"app.test.yaml"},
+		Paths:     []string{".", "./config"},
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, val, cfg.DB.DsnMain)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kubuskotak/valkyrie
 
 go 1.16
+
+require (
+	github.com/ilyakaznacheev/cleanenv v1.2.5 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+)
